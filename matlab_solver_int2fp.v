c730c990ae56552df98afb4545d8eb65f3ddd727
kd-496 <152751109+kd-496@users.noreply.github.com>
2024-04-26 10:40:48 -0400

    Update matlab_solver_int2fp.v

--- matlab_solver_int2fp.v
+++ matlab_solver_int2fp.v
@@ -11,36 +11,95 @@ parameter M = 32'h00000005;  // Mass of Earth in kg (5.972e24)
 parameter dt = 10;           // Time step in seconds
 
 // Initial position and velocity (floating-point format)
-reg signed [15:0] x_fp, y_fp;
+reg signed [15:0] x_fp, y_fp, vx_fp, vy_fp;
 // Initial position and velocity (integer format)
-reg [31:0] x_int, y_int;
+reg [31:0] x_int, y_int, vx_int, vy_int;
 // Conversion from floating-point to integer
 Int2Fp int2fp_x (.iInteger(x_int), .oA(x_fp));
 Int2Fp int2fp_y (.iInteger(y_int), .oA(y_fp));
+Int2Fp int2fp_vx (.iInteger(vx_int), .oA(vx_fp));
+Int2Fp int2fp_vy (.iInteger(vy_int), .oA(vy_fp));
+// Conversion from integer to floating-point
+Fp2Int fp2int_x (.iA(x_fp), .oInteger(x_int));
+Fp2Int fp2int_y (.iA(y_fp), .oInteger(y_int));
+Fp2Int fp2int_vx (.iA(vx_fp), .oInteger(vx_int));
+Fp2Int fp2int_vy (.iA(vy_fp), .oInteger(vy_int));
 
 // Other declarations...
+reg [7:0] steps;  // Number of steps for simulation
+reg [31:0] r, ax, ay; // Radial distance and acceleration components
+reg [31:0] i; // Loop counter
 
 // Euler's method to update position and velocity
 always @(posedge clk) begin
     if (rst) begin
         x_fp <= $signed(32'h00026F90); // Reset x position in floating-point format
         y_fp <= 32'h00000000;          // Reset y position in floating-point format
+        vx_fp <= 32'h00000000;         // Reset x velocity in floating-point format
+        vy_fp <= $signed(sqrt(G * M / x_fp)); // Circular orbit velocity in floating-point format
         x_int <= 6.371e6 + 400000;     // Reset x position in integer format
         y_int <= 0;                     // Reset y position in integer format
+        vx_int <= 0;                   // Reset x velocity in integer format
+        vy_int <= sqrt(G * M / x_int); // Circular orbit velocity in integer format
         // Conversion from integer to floating-point
         int2fp_x.iInteger <= x_int;
         int2fp_y.iInteger <= y_int;
+        int2fp_vx.iInteger <= vx_int;
+        int2fp_vy.iInteger <= vy_int;
     end else begin
-        // Other calculations...
-        // Update positions in integer format
-        x_int <= x_fp;
-        y_int <= y_fp;
+        // Calculate radial distance
+        r <= $signed(sqrt(x_fp*x_fp + y_fp*y_fp));
+        // Calculate acceleration components
+        ax <= -G * M * x_fp / (r*r*r);
+        ay <= -G * M * y_fp / (r*r*r);
+        // Update velocities
+        vx_fp <= vx_fp + ax * dt;
+        vy_fp <= vy_fp + ay * dt;
+        // Update positions
+        x_fp <= x_fp + vx_fp * dt;
+        y_fp <= y_fp + vy_fp * dt;
         // Conversion from integer to floating-point
         int2fp_x.iInteger <= x_int;
         int2fp_y.iInteger <= y_int;
+        int2fp_vx.iInteger <= vx_int;
+        int2fp_vy.iInteger <= vy_int;
     end
 end
 
 // Other declarations...
+reg [31:0] X_array[0:steps-1]; // Array for storing X positions
+reg [31:0] Y_array[0:steps-1]; // Array for storing Y positions
+reg [31:0] T;  // Total time in seconds (1 hour)
+
+// Simulation time
+always @(posedge clk) begin
+    if (rst) begin
+        steps <= T / dt; // Calculate number of steps for simulation
+        i <= 0; // Initialize loop counter
+    end else begin
+        // Store positions for plotting
+        if (i < steps) begin
+            X_array[i] <= x_int;
+            Y_array[i] <= y_int;
+            i <= i + 1;
+        end
+    end
+end
+
+// Other calculations...
+// Plotting the orbit
+always @(posedge clk) begin
+    if (rst) begin
+        X <= 0; // Reset X position output
+        Y <= 0; // Reset Y position output
+    end else begin
+        if (i == steps) begin
+            X <= X_array[steps-1]; // Output X position
+            Y <= Y_array[steps-1]; // Output Y position
+        end
+    end
+end
 
 endmodule
+
+// Include the floating-point modules here (Int2Fp, Fp2Int, FpShift, FpNegate, FpAbs, FpCompare, FpInvSqrt, FpMul, FpAdd)
